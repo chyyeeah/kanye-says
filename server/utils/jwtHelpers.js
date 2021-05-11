cc88ad5cd12681800a3a6ca6fa3ed05ae5f74dc2
Wilson Cheah <wilsoncheah@gmail.com>
2021-05-11 12:02:50 -0700

    moved jwt helper functions to a jwtHelpers.js file

---
+++ server/utils/jwtHelpers.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+module.exports.generateAccessToken = data => jwt.sign(data, process.env.TOKEN_SECRET, { expiresIn: '432000s' });
+module.exports.authenticateToken = (req, res, next) => {
+  const token = req.cookies.token;
+  if (token === null || token === undefined) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+    if (err) {
+      console.log('err', err);
+      return res.sendStatus(403);
+    };
+    req.user = user;
+    console.log('user', req.user);
+    next();
+  })
+};
