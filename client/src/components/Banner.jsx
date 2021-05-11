799996dbc106e0d3a559b381939f0475dfcf3836
Wilson Cheah <wilsoncheah@gmail.com>
2021-05-11 13:51:59 -0700

    - added links to /register, /login, /logout - added a CssBaseline

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom'
+import React, { useState } from 'react';
+import { useLocation, Link as RouterLink } from 'react-router-dom'
 import {
   AppBar, Toolbar, IconButton, MenuIcon, Typography,
-  Button, Menu } from '@material-ui/core';
+  Button, Menu, CssBaseline } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
@@ -24,26 +24,29 @@ export default ({ isAuthenticated }) => {
   let loginOrRegister;
 
   if (!isAuthenticated && location.pathname === '/login') {
-    loginOrRegister = <Button color="inherit">Register</Button>;
+    loginOrRegister = <Button color="inherit" component={RouterLink} to='/register'>Register</Button>;
   } else if (!isAuthenticated && location.pathname === '/register') {
-    loginOrRegister = <Button color="inherit">Login</Button>;
+    loginOrRegister = <Button color="inherit" component={RouterLink} to='/login'>Login</Button>;
   } else {
-    loginOrRegister = <Button color="inherit">Logout</Button>;;
+    loginOrRegister = <Button color="inherit" component={RouterLink} to='/logout'>Logout</Button>;;
   }
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <img src='/img/icon.png' />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            kanye says
-          </Typography>
-          {loginOrRegister}
-        </Toolbar>
-      </AppBar>
-    </div>
+    <>
+      <CssBaseline />
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <img src='/img/icon.png' />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              kanye says
+            </Typography>
+            {loginOrRegister}
+          </Toolbar>
+        </AppBar>
+      </div>
+    </>
   );
 };
