cf4c81521bae892d5a438120932299bc0780287c
Wilson Cheah <wilsoncheah@gmail.com>
2021-05-11 13:51:38 -0700

    moved state up one level from Quote component

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,12 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
+import axios from 'axios';
 import Quote from './Quote.jsx';
 import Banner from './Banner.jsx';
 import checkForToken from '../utils/checkForToken';
+import deleteCookie from '../utils/deleteCookie';
 
 export default ({ isAuthenticated, setIsAuthenticated }) => {
+  const [ quote, setQuote ] = useState('');
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      axios.get('/quote')
+      .then(res => {
+        setQuote(res.data.quote);
+      })
+      .catch(err => {
+        setIsAuthenticated(false);
+        deleteCookie('token');
+      });
+    }
+  }, []);
+
   const authenticated = isAuthenticated
-    ? <Quote setIsAuthenticated={setIsAuthenticated} />
+    ? quote ? <Quote quote={quote} /> : 'loading..'
     : <Redirect to='/login' />
     ;
 
